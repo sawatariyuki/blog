28b91e1efa07de49066f220a3242aa8e39cf71c1
sawatariyuki <cslzf96226@gmain.com>
2018-11-21 13:36:05 +0800

    增加地图注解

--- 1811/[181121] mapjs/XYZ.md
+++ 1811/[181121] mapjs/XYZ.md
@@ -1,4 +1,4 @@
-# openlayers调用江苏天地图（仅矢量底图）
+# openlayers调用江苏天地图（矢量底图 + 注解）
 江苏天地图-特色功能-综合服务: http://www.mapjs.com.cn/map/pub/index  
 OpenLayers Workshop install: https://openlayers.org/workshop/en/
 
@@ -13,9 +13,9 @@ OpenLayers Workshop install: https://openlayers.org/workshop/en/
 wkid指明```projection```使用```'EPSG:4326'```
 
 ## url
-   综合服务的江苏省矢量底图请求地址为  
-   （某个tile）  
-   http://218.2.231.245/mapjs2/rest/services/MapJS/js_sldt_2017/MapServer/tile/9/82/427
+综合服务的江苏省矢量底图请求地址为  
+（某个tile）  
+http://218.2.231.245/mapjs2/rest/services/MapJS/js_sldt_2017/MapServer/tile/9/82/427
 
 对比openLayers使用XYZSource的TileLayer发现请求地址的后三个值(9/82/427)为
 ```
@@ -27,7 +27,12 @@ z = abs(_z)
 y = abs(abs(_y) - 2**(abs(_z) - 2) - 1)
 x = abs(_x)
 ```
-##相关文件  
+
+## 注解
+将Layer中的请求地址换成下面的即可  
+http://218.2.231.245/mapjs2/rest/services/MapJS/js_slzj_2017/MapServer/tile/z/y/x  
+
+## 相关文件
 [Json返回值 - response.json](./files/response.json)  
 [OpenLayers Workshop - index.html](./files/index.html)  
 [OpenLayers Workshop - main.js](./files/main.js)  
--- 1811/[181121] mapjs/files/main.js
+++ 1811/[181121] mapjs/files/main.js
@@ -4,29 +4,39 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import XYZSource from 'ol/source/XYZ';
 
+let mapLayer = new TileLayer({
+  source: new XYZSource({
+    extent: [116.10358013377254, 30.710719079012677, 122.09030402444137, 35.21265930204362],
+    tileSize: [256, 256],
+    tileUrlFunction : function (xyz, obj1, obj2) {
+        var z = Math.abs(xyz[0])
+        var y = Math.abs(Math.abs(xyz[2]) - Math.pow(2, z - 2) - 1)
+        var x = Math.abs(xyz[1])
+        var url = 'http://218.2.231.245/mapjs2/rest/services/MapJS/js_sldt_2017/MapServer/tile/'
+          + z + '/' + y + '/'+ x
+        return url
+    }
+  })
+})
+
+let markLayer = new TileLayer({
+  source: new XYZSource({
+    extent: [116.10358013377254, 30.710719079012677, 122.09030402444137, 35.21265930204362],
+    tileSize: [256, 256],
+    tileUrlFunction : function (xyz, obj1, obj2) {
+        var z = Math.abs(xyz[0])
+        var y = Math.abs(Math.abs(xyz[2]) - Math.pow(2, z - 2) - 1)
+        var x = Math.abs(xyz[1])
+        var url = 'http://218.2.231.245/mapjs2/rest/services/MapJS/js_slzj_2017/MapServer/tile/'
+          + z + '/' + y + '/'+ x
+        return url
+    }
+  })
+})
+
 new Map({
   target: 'map-container',
-  layers: [
-    new TileLayer({
-      source: new XYZSource({
-        // url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
-
-        // url: 'http://218.2.231.245/mapjs2/rest/services/MapJS/js_sldt_2017/MapServer/tile/{z}/{y}/{x}',
-        extent: [116.10358013377254, 30.710719079012677, 122.09030402444137, 35.21265930204362],
-        // tileSize: [256, 256],
-        // origin: [-180, 90]
-
-        tileUrlFunction : function (xyz, obj1, obj2) {
-            var z = Math.abs(xyz[0])
-            var y = Math.abs(Math.abs(xyz[2]) - Math.pow(2, z - 2) - 1)
-            var x = Math.abs(xyz[1])
-            var url = 'http://218.2.231.245/mapjs2/rest/services/MapJS/js_sldt_2017/MapServer/tile/'
-              + z + '/' + y + '/'+ x
-            return url
-        }
-      })
-    })
-  ],
+  layers: [mapLayer, markLayer],
   view: new View({
     center: [120, 32],
     zoom: 9,
